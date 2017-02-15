8a7874eb1dcb90d619a304e27aff364493b1cd78
Mahdi Kallel <Mahdi Kallel>
2017-02-15 17:22:31 +0100

    server running with connect

--- controllers/Books.js
+++ controllers/Books.js
@@ -3,25 +3,8 @@
 var url = require('url');
 
 var BooksService = require('./BooksService')();
-module.exports = {saveBook, getBookById, getBooksList, updateBook, patchBook,delBook};
+module.exports = {saveBook, getBookById, getBooksList, updateBook, patchBook, delBook};
 
-/*module.exports.getBookById = function getBookById (req, res, next) {
- console.log("getBookById   req.swagger.params ---------" + req.swagger.params);
- Books.getBookById(req.swagger.params, res, next);
- };
-
- module.exports.getBooksList = function getBooksList (req, res, next) {
- console.log("getBooksList  req.swagger.params ---------" + req.swagger.params);
- Books.getBooksList(req.swagger.params, res, next);
- };
-
- };*/
-
-
-/*
- function getBookById(req, res, next) {
- Books.getBookById(req.swagger.params, res, next);
- }*/
 
 function getBooksList(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -30,20 +13,37 @@ function getBooksList(req, res, next) {
 
 function saveBook(req, res, next) {
 
-    console.log("req.body ---------" + req.body.id);
-    console.log("req.body ---------" + req.body.title);
-    console.log("req.body ---------" + req.body.author);
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+
+    console.log("request.method ------" + req.method);
+    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
     res.end(JSON.stringify({success: BooksService.saveBook(req.body), description: "Books added"}));
-    //res.status(200).send();
+
+    //req.post('/user').set('Content-Type', 'application/json').send('{"name":"tj","pet":"tobi"}').end();
+    //res.end();
 }
 
 
 function updateBook(req, res, next) {
 
-    console.log("req.body ---------" + req.body.id);
-    console.log("req.body ---------" + req.body.title);
-    console.log("req.body ---------" + req.body.author);
     var id = req.swagger.params.id.value;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
     if (BooksService.find(id)) {
         res.end(JSON.stringify({success: BooksService.updateBook(req.body, id), description: "Books updated"}));
     } else {
@@ -53,9 +53,10 @@ function updateBook(req, res, next) {
 
 
 function getBookById(req, res, next) {
-    console.log("swagger id --" + req.swagger.params.id.value);
     var id = req.swagger.params.id.value; //req.swagger contains the path parameters
     var book = BooksService.find(id);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
     if (book) {
         res.end(JSON.stringify(book));
     } else {
@@ -66,39 +67,46 @@ function getBookById(req, res, next) {
 
 function patchBook(req, res, next) {
 
-    console.log("----------------Attribute to patch-------------------");
-    console.log("req.body.id ---------" + req.body.id);
-    console.log("req.body.title ---------" + req.body.title);
-    console.log("req.body.author ---------" + req.body.author);
-
-    console.log("----------------Old book-------------------");
     var id = req.swagger.params.id.value;
     var book = BooksService.find(id);
-
-    console.log("book ---------" + book.id);
-    console.log("book ---------" + book.title);
-    console.log("book ---------" + book.author);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
     if (BooksService.updateBook(req.body, id) === 1) {
-     res.end(JSON.stringify({success: BooksService.updateBook(req.body, id), description: "Book patched"}));
-     } else {
-     res.end(JSON.stringify({  message: "Book undefined "}));
-     }
-}
-
-
-
-function delBook(req, res, next) {
-    var id = req.swagger.params.id.value;
-   /* var book = BooksService.find(id);
-
-   console.log("req.swagger.params.id.value ---------" + req.swagger.params.id.value);
-    console.log("book ---------" + book.title);
-    console.log("book ---------" + book.author);*/
-    if (BooksService.find(id)) {
-        res.end(JSON.stringify({success: BooksService.delBook(id), description: "Book deleted"}));
+        res.end(JSON.stringify({success: BooksService.updateBook(req.body, id), description: "Book patched"}));
     } else {
-        res.end(JSON.stringify({  message: "Book undefined "}));
+        res.end(JSON.stringify({message: "Book undefined "}));
     }
 }
 
 
+function delBook(req, res, next) {
+
+    var id = req.swagger.params.id.value;
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Content-Type', 'application/json');
+    if (BooksService.find(id)) {
+        res.end(JSON.stringify({success: BooksService.delBook(id), description: "Book deleted"}));
+    } else {
+        res.end(JSON.stringify({message: "Book undefined "}));
+    }
+}
+
+
+/* var book = BooksService.find(id);
+
+ console.log("req.swagger.params.id.value ---------" + req.swagger.params.id.value);
+ console.log("book ---------" + book.title);
+ console.log("book ---------" + book.author);*/
+
+
+/* console.log("----------------Attribute to patch-------------------");
+ console.log("req.body.id ---------" + req.body.id);
+ console.log("req.body.title ---------" + req.body.title);
+ console.log("req.body.author ---------" + req.body.author);
+
+ console.log("----------------Old book-------------------");
+
+ console.log("book ---------" + book.id);
+ console.log("book ---------" + book.title);
+ console.log("book ---------" + book.author);*/
+//console.log("swagger id --" + req.swagger.params.id.value);
